1fa2de6ae43d1515b15d3f0177ffea3c2dbd178f
nobeljb <nobeljulianbintang@gmail.com>
2026-05-27 14:09:45 +0700

    pick the rope

--- Scripts/PickTheRope/pick_the_rope_board.gd
+++ Scripts/PickTheRope/pick_the_rope_board.gd
@@ -11,21 +11,8 @@ enum FeedbackState {
 }
 
 const MAX_ROPES: int = 4
-const PLAYER_FUR_COLOR: Color = Color(0.94, 0.89, 0.8)
-const PLAYER_SPOT_COLOR: Color = Color(0.83, 0.66, 0.44)
-const PLAYER_COLLAR_COLOR: Color = Color(0.28, 0.74, 0.4)
-const NPC_FUR_COLORS: Array[Color] = [
-	Color(0.91, 0.86, 0.77),
-	Color(0.89, 0.83, 0.74),
-	Color(0.87, 0.81, 0.71),
-]
-const NPC_COLLAR_COLORS: Array[Color] = [
-	Color(0.73, 0.42, 0.86),
-	Color(0.94, 0.63, 0.22),
-	Color(0.33, 0.62, 0.9),
-]
 const ROPE_TINT_OFFSETS: Array[float] = [-0.03, 0.02, -0.01, 0.04]
-const TANGLE_CENTER: Vector2 = Vector2(646.0, 334.0)
+const PLAYER_COLLAR_COLOR: Color = Color(0.28, 0.74, 0.4)
 const TARGET_KNOT_OFFSETS := [
 	Vector2(-16.0, -10.0),
 	Vector2(16.0, 8.0),
@@ -76,8 +63,33 @@ const SPREAD_VARIANTS := [
 @export var rope_count: int = 4:
 	set(value):
 		rope_count = clampi(value, 1, MAX_ROPES)
-		_sync_hook_layout()
-		queue_redraw()
+		if is_node_ready():
+			_sync_hook_layout()
+			_sync_rope_visuals()
+			_sync_dog_visuals()
+@export var rope_color: Color = Color(0.9, 0.37, 0.39):
+	set(value):
+		rope_color = value
+		if is_node_ready():
+			_sync_rope_visuals()
+@export var correct_color: Color = Color(0.28, 0.77, 0.39):
+	set(value):
+		correct_color = value
+		if is_node_ready():
+			_sync_rope_visuals()
+@export var wrong_color: Color = Color(0.88, 0.26, 0.23):
+	set(value):
+		wrong_color = value
+		if is_node_ready():
+			_sync_rope_visuals()
+@export var rope_shadow_color: Color = Color(0.09, 0.08, 0.07, 0.24):
+	set(value):
+		rope_shadow_color = value
+		if is_node_ready():
+			_sync_rope_visuals()
+@export var dog_neck_offset: Vector2 = Vector2(0.0, -30.0)
+
+@export_group("Fallback Positions")
 @export var hook_positions: PackedVector2Array = PackedVector2Array([
 	Vector2(916.0, 182.0),
 	Vector2(916.0, 248.0),
@@ -90,14 +102,14 @@ const SPREAD_VARIANTS := [
 	Vector2(196.0, 452.0),
 	Vector2(314.0, 504.0),
 ])
-@export var rope_color: Color = Color(0.9, 0.37, 0.39)
-@export var correct_color: Color = Color(0.28, 0.77, 0.39)
-@export var wrong_color: Color = Color(0.88, 0.26, 0.23)
-@export var rope_shadow_color: Color = Color(0.09, 0.08, 0.07, 0.24)
-@export var rope_width: float = 6.0
-@export var rope_shadow_width: float = 11.0
+@export_group("")
 
-@onready var hook_nodes: Array[PickTheRopeHook] = [$HookA, $HookB, $HookC, $HookD]
+@onready var hook_nodes: Array[PickTheRopeHook] = [$HookA as PickTheRopeHook, $HookB as PickTheRopeHook, $HookC as PickTheRopeHook, $HookD as PickTheRopeHook]
+@onready var rope_nodes: Array[Line2D] = [$Ropes/RopeA as Line2D, $Ropes/RopeB as Line2D, $Ropes/RopeC as Line2D, $Ropes/RopeD as Line2D]
+@onready var rope_shadow_nodes: Array[Line2D] = [$RopeShadows/RopeShadowA as Line2D, $RopeShadows/RopeShadowB as Line2D, $RopeShadows/RopeShadowC as Line2D, $RopeShadows/RopeShadowD as Line2D]
+@onready var dog_slot_nodes: Array[Node2D] = [$DogSlots/DogSlotA as Node2D, $DogSlots/DogSlotB as Node2D, $DogSlots/DogSlotC as Node2D, $DogSlots/DogSlotD as Node2D]
+@onready var dog_visual_nodes: Array[Node2D] = [$DogSlots/DogSlotA/Visuals as Node2D, $DogSlots/DogSlotB/Visuals as Node2D, $DogSlots/DogSlotC/Visuals as Node2D, $DogSlots/DogSlotD/Visuals as Node2D]
+@onready var dog_glow_nodes: Array[Sprite2D] = [$DogSlots/DogSlotA/Glow as Sprite2D, $DogSlots/DogSlotB/Glow as Sprite2D, $DogSlots/DogSlotC/Glow as Sprite2D, $DogSlots/DogSlotD/Glow as Sprite2D]
 
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 var dog_slot_for_hook: Array[int] = []
@@ -128,8 +140,6 @@ func reset_round() -> void:
 	_shuffle_assignments()
 	_build_rope_paths()
 	_reset_feedback_visuals()
-	_sync_hook_layout()
-	queue_redraw()
 
 
 func set_pick_enabled(value: bool) -> void:
@@ -166,17 +176,11 @@ func reveal_result(selected_hook_index: int, correct_hook_index_value: int) -> v
 	if selected_hook_index == correct_hook_index and correct_hook_index >= 0:
 		_play_player_success_feedback()
 
-	queue_redraw()
+	_sync_rope_visuals()
 
 
 func get_correct_hook_index() -> int:
 	return correct_hook_index
-
-
-func _draw() -> void:
-	_draw_tangle_shadow()
-	_draw_ropes()
-	_draw_dogs()
 
 
 func _shuffle_assignments() -> void:
@@ -203,7 +207,6 @@ func _build_rope_paths() -> void:
 	rope_states.clear()
 
 	var knot_points: Array = KNOT_VARIANTS[current_pattern_index]
-
 	for hook_index in range(rope_count):
 		var dog_slot_index: int = dog_slot_for_hook[hook_index]
 		var knot_point: Vector2 = knot_points[hook_index] + TARGET_KNOT_OFFSETS[dog_slot_index]
@@ -218,11 +221,11 @@ func _build_rope_path(hook_index: int, dog_slot_index: int, knot_point: Vector2)
 	var spread_points: Array = SPREAD_VARIANTS[current_pattern_index]
 	var end: Vector2 = _get_dog_neck_anchor(dog_slot_index)
 	var spread_point: Vector2 = spread_points[dog_slot_index] + Vector2(float(hook_index - dog_slot_index) * 4.0, float(hook_index - dog_slot_index) * 4.0)
-	var outward_sign: float = -1.0
-	var start: Vector2 = hook_positions[hook_index] + Vector2(outward_sign * 22.0, 0.0)
+	var hook_position: Vector2 = _get_hook_position(hook_index)
+	var start: Vector2 = hook_position + Vector2(-22.0, 0.0)
 	var hook_exit: Vector2 = start + Vector2(-34.0, clampf((end.y - start.y) * 0.04, -7.0, 7.0))
 	var pole_clear: Vector2 = Vector2(
-		hook_positions[hook_index].x - (94.0 + float(current_pattern_index) * 8.0),
+		hook_position.x - (94.0 + float(current_pattern_index) * 8.0),
 		start.y + float(hook_index - 1) * 4.0
 	)
 	var lane_point: Vector2 = Vector2(
@@ -231,32 +234,9 @@ func _build_rope_path(hook_index: int, dog_slot_index: int, knot_point: Vector2)
 	)
 	var rope_points: PackedVector2Array = PackedVector2Array()
 
-	_append_bezier_segment(
-		rope_points,
-		start,
-		start + Vector2(-14.0, 0.0),
-		hook_exit + Vector2(10.0, 0.0),
-		hook_exit,
-		5
-	)
-
-	_append_bezier_segment(
-		rope_points,
-		hook_exit,
-		hook_exit + Vector2(-18.0, 0.0),
-		pole_clear + Vector2(18.0, clampf((hook_exit.y - pole_clear.y) * 0.16, -10.0, 10.0)),
-		pole_clear,
-		6
-	)
-
-	_append_bezier_segment(
-		rope_points,
-		pole_clear,
-		pole_clear + Vector2(-28.0, 0.0),
-		lane_point + Vector2(34.0, clampf((pole_clear.y - lane_point.y) * 0.18, -16.0, 16.0)),
-		lane_point,
-		6
-	)
+	_append_bezier_segment(rope_points, start, start + Vector2(-14.0, 0.0), hook_exit + Vector2(10.0, 0.0), hook_exit, 5)
+	_append_bezier_segment(rope_points, hook_exit, hook_exit + Vector2(-18.0, 0.0), pole_clear + Vector2(18.0, clampf((hook_exit.y - pole_clear.y) * 0.16, -10.0, 10.0)), pole_clear, 6)
+	_append_bezier_segment(rope_points, pole_clear, pole_clear + Vector2(-28.0, 0.0), lane_point + Vector2(34.0, clampf((pole_clear.y - lane_point.y) * 0.18, -16.0, 16.0)), lane_point, 6)
 
 	var knot_dx: float = knot_point.x - lane_point.x
 	_append_bezier_segment(
@@ -310,106 +290,86 @@ func _cubic_bezier_point(p0: Vector2, p1: Vector2, p2: Vector2, p3: Vector2, t: 
 	)
 
 
-func _draw_tangle_shadow() -> void:
-	pass
+func _reset_feedback_visuals() -> void:
+	_clear_feedback_tween()
+	player_dog_glow = 0.0
+	player_dog_bounce = 0.0
+
+	for index in range(hook_nodes.size()):
+		var hook: PickTheRopeHook = hook_nodes[index]
+		hook.visible = index < rope_count
+		hook.reset_hook(index, _get_neutral_rope_color(index))
+
+	for index in range(rope_states.size()):
+		rope_states[index] = FeedbackState.NEUTRAL
+
+	_sync_hook_layout()
+	_sync_rope_visuals()
+	_sync_dog_visuals()
 
 
-func _draw_ropes() -> void:
-	for index in range(rope_paths.size()):
-		var path: PackedVector2Array = rope_paths[index]
-		var rope_state: int = rope_states[index]
-		var shadow_color: Color = _get_rope_shadow_for_state(rope_state)
-		var line_color: Color = _get_rope_color_for_state(index, rope_state)
+func _sync_hook_layout() -> void:
+	if hook_nodes.is_empty():
+		return
 
-		draw_polyline(path, shadow_color, rope_shadow_width, true)
-		draw_polyline(path, line_color, rope_width, true)
-
-
-func _draw_dogs() -> void:
-	for slot_index in range(rope_count):
-		_draw_dog(slot_index)
+	for index in range(hook_nodes.size()):
+		var hook: PickTheRopeHook = hook_nodes[index]
+		hook.visible = index < rope_count
+		if index >= rope_count:
+			hook.set_pick_enabled(false)
 
 
-func _draw_dog(slot_index: int) -> void:
-	var is_player_dog: bool = slot_index == player_dog_slot_index
-	var origin: Vector2 = dog_slot_positions[slot_index]
-	if is_player_dog:
-		origin.y -= player_dog_bounce
+func _sync_rope_visuals() -> void:
+	for index in range(MAX_ROPES):
+		var has_path: bool = index < rope_paths.size() and index < rope_count
+		var path: PackedVector2Array = rope_paths[index] if has_path else PackedVector2Array()
+		var rope_state: int = rope_states[index] if index < rope_states.size() else FeedbackState.NEUTRAL
 
-	var fur_color: Color = PLAYER_FUR_COLOR if is_player_dog else NPC_FUR_COLORS[slot_index % NPC_FUR_COLORS.size()]
-	var spot_color: Color = PLAYER_SPOT_COLOR if is_player_dog else fur_color.darkened(0.12)
-	var collar_color: Color = PLAYER_COLLAR_COLOR if is_player_dog else NPC_COLLAR_COLORS[slot_index % NPC_COLLAR_COLORS.size()]
+		var rope_line: Line2D = rope_nodes[index]
+		rope_line.visible = has_path
+		rope_line.points = path
+		rope_line.default_color = _get_rope_color_for_state(index, rope_state)
 
-	if is_player_dog:
-		var aura_strength: float = 0.46 + player_dog_glow * 0.54
-		var floor_glow: Color = PLAYER_COLLAR_COLOR
-		floor_glow.a = 0.1 + aura_strength * 0.09
-		draw_circle(origin + Vector2(0.0, 6.0), 48.0 + aura_strength * 10.0, floor_glow)
-
-		var aura_color: Color = PLAYER_COLLAR_COLOR.lightened(0.14)
-		aura_color.a = 0.12 + aura_strength * 0.14
-		draw_circle(origin + Vector2(0.0, -22.0), 62.0 + aura_strength * 18.0, aura_color)
-
-		var ring_color: Color = correct_color.lightened(0.08)
-		ring_color.a = 0.28 + player_dog_glow * 0.22
-		draw_arc(origin + Vector2(0.0, -24.0), 46.0 + player_dog_glow * 5.0, 0.0, TAU, 36, ring_color, 3.0)
-		_draw_player_marker(origin + Vector2(0.0, -102.0 - player_dog_glow * 6.0), aura_strength)
-
-	draw_circle(origin + Vector2(0.0, 8.0), 34.0, Color(0.15, 0.11, 0.09, 0.12))
-	draw_colored_polygon(PackedVector2Array([
-		origin + Vector2(-30.0, -58.0),
-		origin + Vector2(-46.0, -84.0),
-		origin + Vector2(-18.0, -72.0),
-	]), fur_color)
-	draw_colored_polygon(PackedVector2Array([
-		origin + Vector2(30.0, -58.0),
-		origin + Vector2(46.0, -84.0),
-		origin + Vector2(18.0, -72.0),
-	]), fur_color)
-	draw_circle(origin + Vector2(0.0, -48.0), 28.0, fur_color)
-	draw_circle(origin + Vector2(-12.0, -54.0), 10.0, spot_color)
-	draw_circle(origin + Vector2(0.0, -8.0), 34.0, fur_color)
-	draw_rect(Rect2(origin + Vector2(-26.0, -30.0), Vector2(52.0, 10.0)), collar_color, true)
-	draw_circle(origin + Vector2(0.0, -25.0), 5.0, collar_color.lightened(0.18))
-	draw_circle(origin + Vector2(-9.0, -52.0), 3.0, Color(0.15, 0.12, 0.1))
-	draw_circle(origin + Vector2(9.0, -52.0), 3.0, Color(0.15, 0.12, 0.1))
-	draw_circle(origin + Vector2(0.0, -42.0), 4.0, Color(0.18, 0.11, 0.1))
-	draw_arc(origin + Vector2(0.0, -36.0), 10.0, 0.35, PI - 0.35, 16, Color(0.36, 0.21, 0.16), 3.0)
-	draw_rect(Rect2(origin + Vector2(-23.0, 8.0), Vector2(10.0, 28.0)), fur_color.darkened(0.14), true)
-	draw_rect(Rect2(origin + Vector2(-5.0, 10.0), Vector2(10.0, 28.0)), fur_color.darkened(0.14), true)
-	draw_rect(Rect2(origin + Vector2(13.0, 8.0), Vector2(10.0, 28.0)), fur_color.darkened(0.14), true)
-	draw_arc(origin + Vector2(30.0, -2.0), 20.0, -0.7, 0.8, 18, spot_color, 5.0)
+		var shadow_line: Line2D = rope_shadow_nodes[index]
+		shadow_line.visible = has_path
+		shadow_line.points = path
+		shadow_line.default_color = _get_rope_shadow_for_state(rope_state)
 
 
-func _draw_player_marker(center: Vector2, aura_strength: float) -> void:
-	var badge_shadow: Color = Color(0.08, 0.09, 0.08, 0.18)
-	draw_circle(center + Vector2(0.0, 8.0), 18.0 + aura_strength * 3.0, badge_shadow)
+func _sync_dog_visuals() -> void:
+	for index in range(dog_slot_nodes.size()):
+		var is_active: bool = index < rope_count
+		var is_player_dog: bool = index == player_dog_slot_index
+		dog_slot_nodes[index].visible = is_active
+		var visual_y: float = -player_dog_bounce if is_player_dog else 0.0
+		dog_visual_nodes[index].position = Vector2(0.0, visual_y)
 
-	var badge_outline: Color = PLAYER_COLLAR_COLOR.darkened(0.18)
-	var badge_fill: Color = PLAYER_COLLAR_COLOR.lightened(0.08)
-	var badge_inner: Color = Color(0.96, 0.99, 0.95)
-	var pointer_color: Color = PLAYER_COLLAR_COLOR.lightened(0.02)
-	draw_colored_polygon(PackedVector2Array([
-		center + Vector2(-9.0, 12.0),
-		center + Vector2(0.0, 28.0 + aura_strength * 2.0),
-		center + Vector2(9.0, 12.0),
-	]), pointer_color)
-	draw_circle(center, 17.0 + aura_strength * 2.0, badge_outline)
-	draw_circle(center, 14.0 + aura_strength * 2.0, badge_fill)
-	draw_circle(center, 10.5 + aura_strength * 1.4, badge_inner)
-	_draw_marker_paw(center + Vector2(0.0, 1.0), PLAYER_COLLAR_COLOR.darkened(0.18))
+		var glow: Sprite2D = dog_glow_nodes[index]
+		glow.visible = is_active and is_player_dog
+		var glow_color: Color = PLAYER_COLLAR_COLOR
+		glow_color.a = 0.18 + player_dog_glow * 0.18
+		glow.modulate = glow_color
+		glow.scale = Vector2.ONE * (1.35 + player_dog_glow * 0.18)
 
 
-func _draw_marker_paw(center: Vector2, color: Color) -> void:
-	draw_circle(center + Vector2(0.0, 4.0), 4.2, color)
-	draw_circle(center + Vector2(-5.0, -3.0), 2.1, color)
-	draw_circle(center + Vector2(-1.6, -5.4), 2.1, color)
-	draw_circle(center + Vector2(1.6, -5.4), 2.1, color)
-	draw_circle(center + Vector2(5.0, -3.0), 2.1, color)
+func _get_hook_position(index: int) -> Vector2:
+	if index < hook_nodes.size() and is_instance_valid(hook_nodes[index]):
+		return hook_nodes[index].position
+	if index < hook_positions.size():
+		return hook_positions[index]
+	return Vector2.ZERO
+
+
+func _get_dog_slot_position(index: int) -> Vector2:
+	if index < dog_slot_nodes.size() and is_instance_valid(dog_slot_nodes[index]):
+		return dog_slot_nodes[index].position
+	if index < dog_slot_positions.size():
+		return dog_slot_positions[index]
+	return Vector2.ZERO
 
 
 func _get_dog_neck_anchor(slot_index: int) -> Vector2:
-	return dog_slot_positions[slot_index] + Vector2(0.0, -30.0)
+	return _get_dog_slot_position(slot_index) + dog_neck_offset
 
 
 func _get_neutral_rope_color(index: int) -> Color:
@@ -451,33 +411,6 @@ func _get_rope_shadow_for_state(rope_state: int) -> Color:
 			return rope_shadow_color
 
 
-func _reset_feedback_visuals() -> void:
-	_clear_feedback_tween()
-	_set_player_dog_glow(0.0)
-	_set_player_dog_bounce(0.0)
-
-	for index in range(hook_nodes.size()):
-		var hook: PickTheRopeHook = hook_nodes[index]
-		hook.position = hook_positions[index]
-		hook.visible = index < rope_count
-		hook.reset_hook(index, _get_neutral_rope_color(index))
-
-	for index in range(rope_states.size()):
-		rope_states[index] = FeedbackState.NEUTRAL
-
-
-func _sync_hook_layout() -> void:
-	if not is_node_ready() or hook_nodes.is_empty():
-		return
-
-	for index in range(hook_nodes.size()):
-		var hook: PickTheRopeHook = hook_nodes[index]
-		hook.position = hook_positions[index]
-		hook.visible = index < rope_count
-		if index >= rope_count:
-			hook.set_pick_enabled(false)
-
-
 func _play_player_success_feedback() -> void:
 	feedback_tween = create_tween()
 	feedback_tween.tween_method(_set_player_dog_glow, 0.0, 1.0, 0.12)
@@ -495,12 +428,12 @@ func _clear_feedback_tween() -> void:
 
 func _set_player_dog_glow(value: float) -> void:
 	player_dog_glow = value
-	queue_redraw()
+	_sync_dog_visuals()
 
 
 func _set_player_dog_bounce(value: float) -> void:
 	player_dog_bounce = value
-	queue_redraw()
+	_sync_dog_visuals()
 
 
 func _on_feedback_finished() -> void:
