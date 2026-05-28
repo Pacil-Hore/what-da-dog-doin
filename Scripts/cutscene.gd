a30d013cb26dd70089d660818468f26781283783
Kae20042005 <farrelilyasa200405@gmail.com>
2026-05-29 03:55:07 +0700

    add cutscene

--- Scripts/cutscene.gd
+++ Scripts/cutscene.gd
@@ -1,15 +1,13 @@
 extends Control
 
 @export var cutscene_title: String = "Cutscene"
-@export var cutscene_body: String = "..."
 
-@onready var title_label: Label = $VBox/TitleLabel
-@onready var body_label: Label = $VBox/BodyLabel
+@onready var video_player: VideoStreamPlayer = $VideoStreamPlayer
 
 func _ready():
-	title_label.text = cutscene_title
-	body_label.text = cutscene_body
-	$VBox/ContinueButton.pressed.connect(_on_continue)
+	
+	$ContinueButton.pressed.connect(_on_continue)
+	video_player.finished.connect(_on_continue)
 
 func _on_continue():
 	AppManager.story_cutscene_done()
